e40d123e36c660de1466b7f0fe00da53eb20c6f2
Valentin Pirgach <valentyn.pirhach@customertimes.com>
2017-08-20 15:23:44 +0300

    Changes for sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
-  :port => '587',
+  :port => 587,
   :authentication => :plain,
-  :user_name => ENV['SANDGRID_USERNAME'],
-  :password => ENV['SANDGRID_PASSWORD'],
-  :domain => 'herokuc.com',
   :enable_starttls_auto => true
 }
