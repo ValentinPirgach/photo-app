c9818bd804e2c69c1a1eccbdeca93fb77118e016
Valentin Pirgach <valentyn.pirhach@customertimes.com>
2017-08-20 14:11:31 +0300

    Email functionality for signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def full_name
+    ("#{first_name} #{last_name}").strip if first_name.present? || last_name.present?
+    "Anonymous"
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => ENV['SANDGRID_USERNAME'],
+  :password => ENV['SANDGRID_PASSWORD'],
+  :domain => 'herokuc.com',
+  :enable_starttls_auto => true
+}
